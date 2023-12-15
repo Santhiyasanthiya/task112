9374104d3d0df01d73191438c6889bc0f5204021
Santhiya .K <103623501+Santhiyasanthiya@users.noreply.github.com>
2023-12-16 00:07:04 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,45 @@
+// // es 5
+
+// function greetCustomer(){
+//     console.log("hello santhiya")
+// }
+// greetCustomer()
+
+
+// function driving (age){
+//     return age >18 ? true : false
+// }
+// var person1 = driving(16)
+// console.log(person1)
+// var person2 = driving(25)
+// console.log(person2)
+
+// var q1 = 5;
+// var q2 = "5";
+
+// console.log(q1 == q2);
+// console.log(q1 === q2);
+
+// var arr = ["apple", "orange", "banana", true, 10]
+// console.log(arr)
+// console.log(arr.length)
+// console.log(arr.indexOf("apple"))
+// console.log(arr[2])
+
+// function double(n) {
+//     return n * 2;
+//   }
+//   console.log(double(10))
+//   console.log(double(20))
+//   console.log(double(30))
+
+//   const sum = (a, b) => a + b;
+//   console.log(sum(10,20))
+
+  q1 = [100, 200];
+  var q3 = [...q1];
+  var q4 = [...q3, 500, 600]
+  console.log(q3)
+  console.log(q4)
+
+  
